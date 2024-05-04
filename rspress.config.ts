2c021b06badc4709349b351f120b613fa224df12
nguyenbatranvan <nguyenbatranvan96@gmail.com>
2024-05-04 20:32:48 +0700

    update deploy workflow

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,17 +3,14 @@ import {defineConfig} from 'rspress/config';
 
 export default defineConfig({
     root: path.join(__dirname, 'docs'),
-    title: 'Rspress',
-    description: 'Rspack-based Static Site Generator',
+    title: 'Vite multiple assets',
+    description: 'Support vite to run with multiple directory public directory.',
     icon: '/logo.png',
     logo: {
         light: '/logo.png',
         dark: '/logo.png',
     },
     logoText: 'Vite Multiple Public',
-    markdown: {
-        checkDeadLinks: true,
-    },
     themeConfig: {
         enableScrollToTop: true,
         enableContentAnimation: true,
